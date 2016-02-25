e685184b25deaafa5e5dca4ebc16842fc071acd7
Taeho Sung <taehosung1994@gmail.com>
2016-02-25 00:16:41 -0500

    pushing before merging

--- tabular_page.php
+++ tabular_page.php
@@ -34,7 +34,7 @@ $(function(){
   console.log("test");
   if (url.match('#')) {
     $('.nav-tabs a[href=#'+url.split('#')[1]+']').tab('show');
-    $('.nav-tabs a[href=#'+url.split('#')[1]+']').parent().addClass('active');
+    console.log($('.nav-tabs a[href=#'+url.split('#')[1]+']').parent());
   }
 
   setTimeout(function() {
